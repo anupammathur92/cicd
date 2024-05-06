a01209bc8d7c9761e75c099c879688fa5a5fa830
Your Name <Your Email>
2024-05-06 10:57:02 +0530

    removed use of composer. updated inex.php file

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-require 'vendor/autoload.php';
-use Ramsey\Uuid\Uuid;
 try{
-    echo "this is cicd";
-    echo "<br>";
-    echo "so, this is a success<br>";
-    echo "hi, this is anupam mathur";
-    echo "<br>";
-    echo "it worked.. ";
-    echo "hello";
-    $uuid = Uuid::uuid4();
-    echo "<br>UUID: ".$uuid->toString();
+    echo "this is aws codepipeline";
 }catch(Exception $e){
     print_r($e->message);
 }
