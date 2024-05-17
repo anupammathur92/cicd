870684e168b7d1aca912673dfb506f23917a4b3e
Anupam Mathur <er.anupam.mathur@gmail.com>
2024-05-17 16:21:28 +0530

    code updated

--- scripts/server_startup.sh
+++ scripts/server_startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 627528354366.dkr.ecr.us-east-1.amazonaws.com
 sudo docker pull 627528354366.dkr.ecr.us-east-1.amazonaws.com/codepipelineecr:latest
-sudo bash -c 'cd /var/www/html/myapp/ && docker run --name myphpapp -d 627528354366.dkr.ecr.us-east-1.amazonaws.com/codepipelineecr:latest'
+sudo bash -c 'cd /var/www/html/myapp/ && docker run -p 8080:80 --name myphpapp -d 627528354366.dkr.ecr.us-east-1.amazonaws.com/codepipelineecr:latest'
