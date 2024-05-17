8372d0bbc8d47dea76d30933fd9d20b33424b5a9
Anupam Mathur <er.anupam.mathur@gmail.com>
2024-05-17 16:26:28 +0530

    updated index.php file

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    echo "this is my first successful AWS codepipeline with v2 of got as source in AWS...";
+    echo "this is my first successful AWS codepipeline with v2 of got as source in AWS...\n Finally it is successful, now...";
 }catch(Exception $e){
     print_r($e->message);
 }
